78fe20d8410159c21b14b51f3c7c90050d9ab53c
emeye-seojh <seojh@emeye.co.kr>
2021-10-26 16:01:14 +0900

    TEMP

--- src/main/java/com/example/demo/TemptestApplication.java
+++ src/main/java/com/example/demo/TemptestApplication.java
@@ -3,9 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * {@summary This is invalid java doc.}
+ * @author ASUS
+ *
+ */
 @SpringBootApplication
 public class TemptestApplication {
-
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TemptestApplication.class, args);
 	}
