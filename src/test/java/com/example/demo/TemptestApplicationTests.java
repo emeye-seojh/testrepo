9701373588af170e1cd15dff958e078e3867c452
emeye-seojh <seojh@emeye.co.kr>
2021-10-26 10:54:08 +0900

    테스트 두번째

--- src/test/java/com/example/demo/TemptestApplicationTests.java
+++ src/test/java/com/example/demo/TemptestApplicationTests.java
@@ -1,15 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class TemptestApplicationTests {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Test
 	void contextLoads() throws Exception {
 		int i = 0;
 		if(i == 0) return;
+	}
+
+	@Test
+	void fortemp() throws Exception {
+		for(int i = 0 ; i < 100; i ++) {
+			logger.debug("for : " + i);
+		}
 	}
 
 }
